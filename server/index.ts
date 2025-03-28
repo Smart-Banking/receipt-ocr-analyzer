e75a56ec6e219f81c6933c777027d8bfc5b43536
Replit <replit@replit.com>
2025-03-28 18:12:57 +0000

    Добавена поддръжка за .env файлове и документация

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,10 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import * as dotenv from 'dotenv';
+
+// Зареждане на .env файла в началото на приложението
+dotenv.config();
 
 const app = express();
 app.use(express.json());
